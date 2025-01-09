ba5286f29a33f87c1f459f3e14074a54451b0275
DanteLong <jlong343@gatech.edu>
2025-01-08 23:59:57 -0500

    25

--- assets/phoneScreen.js
+++ assets/phoneScreen.js
@@ -1,6 +1,7 @@
 // 手机端显示navigation
 const toggleButton = document.getElementById('openMenu');
 const body = document.body;
+const navigationBar = document.getElementsByClassName('navigation-bar')[0];
 const fixedPage = document.getElementById('fixedPage');
 const closeButton = document.getElementById('closeMenu');
 
@@ -9,6 +10,7 @@ toggleButton.addEventListener('click', () => {
     fixedPage.classList.add('show');
     body.style.overflow = "hidden";
     toggleButton.style.display = "none";
+    navigationBar.style.position = "fixed";
     closeButton.style.display = "block"; // 显示固定页面
 });
 
@@ -17,5 +19,6 @@ closeButton.addEventListener('click', () => {
     fixedPage.classList.remove('show'); // 隐藏固定页面
     body.style.overflow = "";
     toggleButton.style.display = "block";
+    navigationBar.style.position = "Absolute";
     closeButton.style.display = "none";
 });
